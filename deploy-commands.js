c1dd5ba78d61867984084005ca1e52787fbb31ae
1122055-maullanayusufmuharram <150674339+1122055-maullanayusufmuharram@users.noreply.github.com>
2025-12-09 11:18:44 +0700

    Add deploy-commands.js for Discord bot commands

---
+++ deploy-commands.js
@@ -0,0 +1,25 @@
+import { REST, Routes } from "discord.js";
+import fs from "fs";
+import dotenv from "dotenv";
+dotenv.config();
+
+const commands = [];
+
+const commandFiles = fs.readdirSync("./commands").filter(f => f.endsWith(".js"));
+for (const file of commandFiles) {
+  const cmd = await import(`./commands/${file}`);
+  commands.push(cmd.default.data.toJSON());
+}
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+try {
+  console.log("Uploading slash commands…");
+  await rest.put(
+    Routes.applicationCommands(process.env.CLIENT_ID),
+    { body: commands }
+  );
+  console.log("Done!");
+} catch (error) {
+  console.error(error);
+}
